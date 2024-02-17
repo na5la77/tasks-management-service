eb1ec75d8005412e82c47b25c30c5c779a26ba1e
Shadi Nakhla <shadinakhla13@gmail.com>
2024-02-17 20:17:33 +0200

    generating generic users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(
       'mongodb+srv://na5la77:Sh1122334455@tasks-management.lh0qwl5.mongodb.net/Tasks-DB?retryWrites=true&w=majority',
     ),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
