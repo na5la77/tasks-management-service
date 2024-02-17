8bbb9f356adcb5f34faa23af891d962f34a0f8aa
Shadi Nakhla <shadinakhla13@gmail.com>
2024-02-17 19:31:59 +0200

    fixing packages and adding mongo connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://na5la77:Sh1122334455@tasks-management.lh0qwl5.mongodb.net/Tasks-DB?retryWrites=true&w=majority',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
